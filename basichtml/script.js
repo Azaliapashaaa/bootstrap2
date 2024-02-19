f64055ceea7cf306c4abb53f1bb42b23f4b4b378
Azaliapashaaa <kayanapasha591@gmail.com>
2024-02-19 11:30:25 +0700

    baru

---
+++ basichtml/script.js
@@ -0,0 +1,57 @@
+// alret
+//alert("hallloooo")
+
+//let kyword 
+//java  di baca dari atas maka output nya akan doo 
+let nama = "jon";
+nama = "doo";
+console.log(nama);
+
+//const keyword 
+const PI = 3.14;
+console.log(PI);
+
+//operator matematika 
+const a = 2;
+const b = 3;
+//pangkat 
+const c = a**b;
+console.log(c);
+//tambah 
+let namad = "azalia";
+let namab = "pasha";
+let fullnama = namad + " "+ namab;
+console.log(fullnama);
+
+// let namadepan = "azalia";
+// let namabelakang = "pasha";
+// let full = ${namadepan} ${namabelakang};
+// console.log(full);
+
+let result = 90; 
+result+=5;
+console.log(result)
+
+//operator perbandingan 
+let x = 10;
+let y = 20;
+console.log(x>y);
+
+let umur = 19;
+if (umur >= 18 && umur<= 25 ) {console.log("udah gede ");}
+else {console.log("masi kecil ");}
+
+for (let kue =3; kue>=1; kue --){
+    console.log("kue udah di mam, sisa" + kue);
+}
+
+// let kue = 3;
+// while (kue>= 0) {
+//     console.log("kue udah di mam, sisa" + kue)
+//     kue--
+// }
+
+//function 
+function tambah(angka1, angka2){
+    
+}
